73508329e2ab40ef428d15f6cbaff7e15de1c8f7
ahmedammarsd <ahmed091999287@gmail.com>
2024-04-18 07:49:45 +0200

    finish lesson to 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import SayHallo from "./components/SayHallo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dummyData = {
+    specilest: "Front-End Developer",
+    experience: 2,
+  };
 
+  const users = [
+    {
+      id: 1,
+      name: "ahmed",
+    },
+    {
+      id: 2,
+      name: "ammar",
+    },
+    {
+      id: 3,
+      name: "snhoory",
+    },
+  ];
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SayHallo name={"ahmed"} age={24} someData={dummyData} users={users} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
